31822bbfc10b1a57b431b5100c4b7cf40adbd615
baidiwangBU <139611558+lili3412@users.noreply.github.com>
2023-07-15 14:21:21 +0800

    finish problem

---
+++ js/waiver.js
@@ -0,0 +1,42 @@
+// Problem 1
+const figures = document.querySelectorAll("figure");
+window.addEventListener("load", function() {
+  for(let i = 0; i < figures.length; i++) {
+    figures[i].classList.add("one-third");
+  }
+});
+
+// Problem 2
+const footer = document.querySelector("footer")
+footer.addEventListener("click", function() {
+  footer.style.display = "none";
+});
+
+// Problem 3
+figures[4].addEventListener("dblclick", function() {
+  figures[4].style.visibility = "hidden";
+});
+
+// Problem 4
+const figcaption = figures[0].querySelector("figcaption");
+
+figcaption.addEventListener("mouseover", function() {
+  figcaption.style.fontFamily = "Cursive";
+});
+
+figcaption.addEventListener("mouseleave", function() {
+  figcaption.style.fontFamily = "";
+});
+
+// Problem 5
+for(let i = 0; i < figures.length; i++) {
+  const figcaption = figures[i].querySelector("figcaption")
+  figcaption.tabIndex = 0;
+  figcaption.addEventListener("focus", function() {
+    figcaption.style.fontFamily = "Cursive";
+  });
+
+  figcaption.addEventListener("blur", function() {
+    figcaption.style.fontFamily = "";
+  });
+}
